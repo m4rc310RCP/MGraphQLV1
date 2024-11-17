7bb63c08d4abb09e517e68df31818e3bbc7caf51
m4rc310 <mlsilva@coamo.com.br>
2024-11-17 10:01:25 -0300

    .

---
+++ src/privateConfig.h
@@ -0,0 +1,9 @@
+#ifndef PRIVATECONFIG_H
+#define PRIVATECONFIG_H
+
+const char * wifi_ssid = "Silva";
+const char * wifi_password = "Dh13ssyc4";
+
+const char * bot_token = "";
+
+#endif //PRIVATECONFIG_H
